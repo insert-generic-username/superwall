c1fdf06b8d6ca79095b929f533e8673511c14c01
Eason Lin <insert-generic-username@lightsteelblue-area.local>
2014-03-10 18:03:36 -0700

    wooo

--- app.rb
+++ app.rb
@@ -6,9 +6,15 @@ require ::File.expand_path('../config/environment',  __FILE__)
 set :app_file, __FILE__
 
 get '/' do
+  posts = Post.order(:created_at).all
+  erb :index, :locals => {:posts => posts}
+end
+
+get '/reverse' do
   posts = Post.reverse_order(:created_at).all
   erb :index, :locals => {:posts => posts}
 end
+
 
 post '/posts' do
   Post.create(:body => params[:body])
